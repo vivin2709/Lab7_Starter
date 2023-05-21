c827d39fefdf8563dde60e7f9f493d9c8c06fdbd
vivin2709 <97651150+vivin2709@users.noreply.github.com>
2023-05-21 14:29:56 -0700

    ToDo's in sw.js completed

--- sw.js
+++ sw.js
@@ -9,8 +9,7 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-
-      return cache.addAll([]);
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -22,20 +21,36 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
+  const url = new URL(event.request.url);
+  const isAsset = RECIPE_URLS.includes(url.pathname)
+  if (isAsset) {
+    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request.URL);
+    }));
+  } else {
+    return;
+  }
+});
+
+
+
+
   // We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
   // list to the cache as they come in.
-  /*******************************/
+/*******************************/
   // This article from Google will help with this portion. Before asking ANY
   // questions about this section, read this article.
   // NOTE: In the article's code REPLACE fetch(event.request.url) with
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
+/*******************************/
+
+
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-});
+
