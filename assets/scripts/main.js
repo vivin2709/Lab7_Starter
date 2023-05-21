96e9241628675dae135788daf3574a4f70ed858f
vivin2709 <97651150+vivin2709@users.noreply.github.com>
2023-05-21 13:47:35 -0700

    Expose completed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,23 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWroler' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('./sw.js')
+        .then(function (registration) {
+          console.log('Service Worker registered successfully');
+
+        })
+        .catch(function (error) {
+          console.log('ServiceWorker registration failed');
+        });
+
+    });
+  }
+  else {
+    console.log('serviceWorker not supported in this browser')
+  }
+
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -68,39 +85,69 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
-  /**************************/
-  // The rest of this method will be concerned with requesting the recipes
-  // from the network
-  // A2. TODO - Create an empty array to hold the recipes that you will fetch
-  // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
-  //            has a great article on them. A promise takes one parameter - A
-  //            function (we call these callback functions). That function will
-  //            take two parameters - resolve, and reject. These are functions
-  //            you can call to either resolve the Promise or Reject it.
-  /**************************/
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+
+  const localRecipes = localStorage.getItem('recipes');
+  if (localRecipes) {
+    return JSON.parse(localRecipes);
+  }
+
+  const recipes = [];
+  return new Promise(async (resolve, reject) => {
+    try {
+      for (const recipeUrl of RECIPE_URLS) {
+        try {
+          const response = await fetch(recipeUrl);
+          const recipe = await response.json();
+          recipes.push(recipe);
+
+        } catch (error) {
+          console.log('Error fetching recipe');
+        }
+      }
+      localStorage.setItem('recipes', JSON.stringify(recipes));
+      resolve(recipes)
+    }
+    catch (error) {
+      reject(error);
+    }
+  });
 }
+
+
+
+/**************************/
+// The rest of this method will be concerned with requesting the recipes
+// from the network
+// A2. TODO - Create an empty array to hold the recipes that you will fetch
+// A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
+//            has a great article on them. A promise takes one parameter - A
+//            function (we call these callback functions). That function will
+//            take two parameters - resolve, and reject. These are functions
+//            you can call to either resolve the Promise or Reject it.
+/**************************/
+// A4-A11 will all be *inside* the callback function we passed to the Promise
+// we're returning
+/**************************/
+// A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+//            declared above
+// A5. TODO - Since we are going to be dealing with asynchronous code, create
+//            a try / catch block. A6-A9 will be in the try portion, A10-A11
+//            will be in the catch portion.
+// A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
+//            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
+//            you must either use "await fetch(...)" or "fetch.then(...)". This
+//            function is using the async keyword so we recommend "await"
+// A7. TODO - For each fetch response, retrieve the JSON from it using .json().
+//            NOTE: .json() is ALSO asynchronous, so you will need to use
+//            "await" again
+// A8. TODO - Add the new recipe to the recipes array
+// A9. TODO - Check to see if you have finished retrieving all of the recipes,
+//            if you have, then save the recipes to storage using the function
+//            we have provided. Then, pass the recipes array to the Promise's
+//            resolve() method.
+// A10. TODO - Log any errors from catch using console.error
+// A11. TODO - Pass any errors to the Promise's reject() function
+
 
 /**
  * Takes in an array of recipes, converts it to a string, and then
